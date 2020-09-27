c7b9d0102be864d09fb9cdf0c734077ddf00eb14
gadaharon <gadaharon91@gmail.com>
2020-09-27 13:02:51 +0300

    add main js file install electron and create window

---
+++ main.js
@@ -0,0 +1,66 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+// Set env
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
+let mainWindow;
+let aboutWindow;
+
+function createMainWindow() {
+  mainWindow = new BrowserWindow({
+    title: 'ImageShrink',
+    width: 500,
+    height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
+    webPreferences: { nodeIntegration: true },
+  });
+
+  // OPEN DEVTOOLS ON START
+  // if (isDev) {
+  //   mainWindow.webContents.openDevTools();
+  // }
+
+  mainWindow.loadFile('./app/index.html');
+}
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+  });
+
+  aboutWindow.setMenuBarVisibility(false);
+  aboutWindow.loadFile('./app/about.html');
+}
+
+app.on('ready', () => {
+  createMainWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  mainWindow.on('closed', () => (mainWindow = null));
+});
+
+const { createMenu } = require('./config');
+
+const menu = createMenu({ isDev, isMac, appName: app.name }, createAboutWindow);
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
